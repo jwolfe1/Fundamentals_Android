02cff9c8c8be1d10b68bc3f560f1e3bfcf03e6f0
jwolfe1 <jwolfe1@student.cscc.edu>
2016-10-04 19:44:25 -0400

    Week_05 commit

---
+++ src/com/JasonWolfe/Week_05/Main.java
@@ -0,0 +1,43 @@
+package com.JasonWolfe.Week_05;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Contact {
+    String name;
+    String email;
+
+    Contact(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+}
+
+class AddressBook{
+    List<Contact> contacts = new ArrayList<>();
+
+    void add(Contact contact){
+        contacts.add(contact);
+    }
+
+    String searchByName(String name){
+        for(Contact entry: contacts){
+            if(entry.name.equals(name)){
+                return entry.email;
+            }
+        }
+        return null;
+    }
+}
+public class Main {
+    public static void main(String[] args){
+        AddressBook myAddressBook = new AddressBook();
+        Contact jason = new Contact("Jason", "jason@email.com");
+        Contact bob = new Contact("Bob", "bob@email.com");
+
+        myAddressBook.add(jason);
+        myAddressBook.add(bob);
+
+        System.out.println("Bob's email address is " + myAddressBook.searchByName("Bob"));
+    }
+}
