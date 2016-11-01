07f3bcb9554b07eecc8641e146b6feb1f9559283
jwolfe1 <jwolfe1@student.cscc.edu>
2016-11-01 19:00:27 -0400

    Week_10 commit

---
+++ src/com/JasonWolfe/Week_10/Main.java
@@ -0,0 +1,35 @@
+package com.JasonWolfe.Week_10;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+class Contact {
+    public String name;
+    public String email;
+
+    public Contact(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name + ", Email: " + email);
+    }
+}
+
+class AddressBook implements Iterable<Contact> {
+    private List<Contact> addressBook = new ArrayList<>();
+
+    public void add(Contact c) {
+        addressBook.add(c);
+    }
+
+    @Override
+    public Iterator<Contact> iterator() {
+        return addressBook.iterator();
+    }
+}
+
+public class Main {
+}
